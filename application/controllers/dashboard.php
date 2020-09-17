b5d12ddcf610ac27841c537b8c8100934c0cd9cb
Icarius <icarius62@gmail.com>
2020-09-17 21:19:34 +0200

    Update dashboard.php

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -2,7 +2,7 @@
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Dashboard extends CI_Controller {
-    
+    public $flush_output = TRUE;
     function __construct() {
         parent::__construct();
 
@@ -20,6 +20,12 @@ class Dashboard extends CI_Controller {
 		}	
         $this->load->view('layouts/main2',$data);
 	}	
-	
+    private function _flush($message = '') {
+        if($this->flush_output === TRUE) {
+            echo $message.'<br/>';
+            ob_flush();
+            flush();
+        }
+    }
 }
 	
