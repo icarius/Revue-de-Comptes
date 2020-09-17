5b40ede758f879eedcbbcfe378110ba7ffaa8ae7
Icarius <icarius62@gmail.com>
2020-09-17 20:08:53 +0200

    Update github_updater.php

--- application/config/github_updater.php
+++ application/config/github_updater.php
@@ -21,7 +21,7 @@ $config['github_branch'] = 'master';
  * NOTE: You should only need to set this initially it will be
  * automatically set by the library after subsequent updates.
  */
-$config['current_commit'] = 'd2605907262c932035ec16bdd2716bcd163622bb';
+$config['current_commit'] = '';
 
 
 /**
