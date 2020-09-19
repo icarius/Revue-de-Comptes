e40c02d0dec7f8b5de0494030e1e122bdcd7b2f0
Icarius <icarius62@gmail.com>
2020-09-19 12:30:36 +0200

    Update Rh_models.php

--- application/models/Rh_models.php
+++ application/models/Rh_models.php
@@ -7,6 +7,7 @@ class Rh_models extends CI_Model{
         // Set table name
         $this->tableAD = 'exportad';
         $this->tableRH = 'exportrem';
+        $this->tablePCPASS = 'exportpcpass';
     }
     
     /*
@@ -184,4 +185,96 @@ class Rh_models extends CI_Model{
         }
         return false;
     }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+    /*
+     * Fetch members data from the database
+     * @param array filter data based on the passed parameters
+     */
+    public function getRowsPCPASS($params = array()){
+        $this->db->select('*');
+        $this->db->from($this->tablePCPASS);
+        
+        if(array_key_exists("where", $params)){
+            foreach($params['where'] as $key => $val){
+                $this->db->where($key, $val);
+            }
+        }
+        
+        if(array_key_exists("returnType",$params) && $params['returnType'] == 'count'){
+            $result = $this->db->count_all_results();
+        }else{
+            if(array_key_exists("matricule", $params)){
+                $this->db->where('matricule', $params['matricule']);
+                $query = $this->db->get();
+                $result = $query->row_array();
+            }
+			else{
+                $this->db->order_by('nom', 'asc');
+                if(array_key_exists("start",$params) && array_key_exists("limit",$params)){
+                    $this->db->limit($params['limit'],$params['start']);
+                }elseif(!array_key_exists("start",$params) && array_key_exists("limit",$params)){
+                    $this->db->limit($params['limit']);
+                }
+                
+                $query = $this->db->get();
+                $result = ($query->num_rows() > 0)?$query->result_array():FALSE;
+            }
+        }
+        
+        // Return fetched data
+        return $result;
+    }
+
+    public function search_pcpass($str){
+        return $this->db->get_where($this->tablePCPASS,$str)->row_array();
+    }
+
+    /*
+     * Insert members data into the database
+     * @param $data data to be insert based on the passed parameters
+     */
+    public function insertPCPASS($data) {
+        if(!empty($data)){
+            $this->db->truncate($this->tablePCPASS);
+            // Insert member data
+            $insert = $this->db->insert_batch($this->tablePCPASS, $data);
+            
+            // Return the status
+            return $insert?$this->db->insert_id():false;
+        }
+        return false;
+    }
+    
+    /*
+     * Update member data into the database
+     * @param $data array to be update based on the passed parameters
+     * @param $condition array filter data
+     */
+    public function updatePCPASS($data, $condition = array()) {
+        if(!empty($data)){
+            // Add modified date if not included
+            //if(!array_key_exists("modified", $data)){
+                //$data['modified'] = date("Y-m-d H:i:s");
+            //}
+            
+            // Update member data
+            $update = $this->db->update($this->tablePCPASS, $data, $condition);
+            
+            // Return the status
+            return $update?true:false;
+        }
+        return false;
+    }
+
+}    
